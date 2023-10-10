66ec59ef2a128f70adb3b84bfcde8e6383e8cbc1
Uncomfy <dimabalabanokda@gmail.com>
2023-10-10 17:00:10 +0300

    Numpy integration check

--- main.cpp
+++ main.cpp
@@ -1,11 +1,43 @@
 #include <pybind11/pybind11.h>
+#include <pybind11/numpy.h>
 
-int add(int i, int j) {
-    return i + j;
+#include <cassert>
+#include <iostream>
+
+namespace py = pybind11;
+
+pybind11::array_t<float> increment(pybind11::array_t<float, py::array::c_style | py::array::forcecast> input) {
+    auto buf = input.request();
+    float *ptr = (float *) buf.ptr;
+
+    assert(buf.ndim == 3);
+
+    int batch_size = buf.shape[0];
+    int t_x = buf.shape[1];
+    int t_y = buf.shape[2];
+    float (*arr)[t_x][t_y] = (float (*)[t_x][t_y]) ptr;
+
+    // Create output array
+    py::array_t<float> output = py::array_t<float>(buf.size);
+    // Set shape of output array
+    output.resize({batch_size, t_x, t_y});
+    auto buf_out = output.request();
+    float *ptr_out = (float *) buf_out.ptr;
+    float (*arr_out)[t_x][t_y] = (float (*)[t_x][t_y]) ptr_out;
+
+    for (int i = 0; i < batch_size; i++) {
+        for (int j = 0; j < t_x; j++) {
+            for (int k = 0; k < t_y; k++) {
+                arr_out[i][j][k] = arr[i][j][k] + 1.0f;
+            }
+        }
+    }
+
+    return output;
 }
 
 PYBIND11_MODULE(monotonic_alignment_cpp, m) {
-    m.doc() = "pybind11 example plugin"; // optional module docstring
+    m.doc() = "Monotonic alignment written in C++"; // optional module docstring
 
-    m.def("add", &add, "A function that adds two numbers");
+    m.def("increment", &increment, "A function that increments all elements of a numpy array by 1", py::arg("input"));
 }
--- test.py
+++ test.py
@@ -1,3 +1,14 @@
 import monotonic_alignment_cpp
+import numpy as np
 
-print(monotonic_alignment_cpp.add(1, 2))
+batch_size = 2
+t_x = 4
+t_y = 10
+
+batch = np.arange(batch_size * t_x * t_y).reshape(batch_size, t_x, t_y).astype(np.float32)
+
+print(batch)
+
+print(monotonic_alignment_cpp.increment(batch))
+
+print(batch)
