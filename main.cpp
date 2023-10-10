e5f5376504559c4c5b76b534ce1c129e86b5903f
Uncomfy <dimabalabanokda@gmail.com>
2023-10-10 17:43:55 +0300

    Implemented maximum path

--- main.cpp
+++ main.cpp
@@ -1,43 +1,117 @@
 #include <pybind11/pybind11.h>
 #include <pybind11/numpy.h>
 
+#include <algorithm>
 #include <cassert>
-#include <iostream>
 
 namespace py = pybind11;
 
-pybind11::array_t<float> increment(pybind11::array_t<float, py::array::c_style | py::array::forcecast> input) {
-    auto buf = input.request();
-    float *ptr = (float *) buf.ptr;
+pybind11::array_t<float> maximum_path(
+    pybind11::array_t<float, py::array::c_style | py::array::forcecast> values,
+    pybind11::array_t<int, py::array::c_style | py::array::forcecast> t_x,
+    pybind11::array_t<int, py::array::c_style | py::array::forcecast> t_y
+) {
+    auto buf_values = values.request();
+    float *ptr_values = (float *) buf_values.ptr;
 
-    assert(buf.ndim == 3);
+    auto buf_t_x = t_x.request();
+    int *ptr_t_x = (int *) buf_t_x.ptr;
 
-    int batch_size = buf.shape[0];
-    int t_x = buf.shape[1];
-    int t_y = buf.shape[2];
-    float (*arr)[t_x][t_y] = (float (*)[t_x][t_y]) ptr;
+    auto buf_t_y = t_y.request();
+    int *ptr_t_y = (int *) buf_t_y.ptr;
+
+    assert(buf_values.ndim == 3);
+    assert(buf_t_x.ndim == 1);
+    assert(buf_t_y.ndim == 1);
+
+    int batch_size = buf_values.shape[0];
+    int max_t_x = buf_values.shape[1];
+    int max_t_y = buf_values.shape[2];
+
+    assert(buf_values.shape[0] == buf_t_x.shape[0]);
+    assert(buf_values.shape[0] == buf_t_y.shape[0]);
+
+    for(int i = 0; i < batch_size; i++) {
+        assert(ptr_t_x[i] <= max_t_x);
+        assert(ptr_t_y[i] <= max_t_y);
+    }
+
+    // Clone the values into new float array
+    float *values_clone = new float[buf_values.size];
+    std::copy(ptr_values, ptr_values + buf_values.size, values_clone);
+
+    // Convert values to 3D array
+    float (*arr_values)[max_t_x][max_t_y] = (float (*)[max_t_x][max_t_y]) values_clone;
 
     // Create output array
-    py::array_t<float> output = py::array_t<float>(buf.size);
+    py::array_t<float> path = py::array_t<float>(buf_values.size);
     // Set shape of output array
-    output.resize({batch_size, t_x, t_y});
-    auto buf_out = output.request();
-    float *ptr_out = (float *) buf_out.ptr;
-    float (*arr_out)[t_x][t_y] = (float (*)[t_x][t_y]) ptr_out;
+    path.resize({batch_size, max_t_x, max_t_y});
+    auto buf_path = path.request();
+    float *ptr_path = (float *) buf_path.ptr;
 
-    for (int i = 0; i < batch_size; i++) {
-        for (int j = 0; j < t_x; j++) {
-            for (int k = 0; k < t_y; k++) {
-                arr_out[i][j][k] = arr[i][j][k] + 1.0f;
+    // Fill with zeros
+    std::fill(ptr_path, ptr_path + buf_path.size, 0.0f);
+
+    // Convert path to 3D array
+    float (*arr_path)[max_t_x][max_t_y] = (float (*)[max_t_x][max_t_y]) ptr_path;
+
+    for(int bid = 0; bid < batch_size; bid++) {
+        // Compute maximum path
+
+        for(int y = 1; y < ptr_t_y[bid]; y++) {
+            arr_values[bid][0][y] += arr_values[bid][0][y - 1];
+        }
+
+        // Cannot start from x other than 0 (because path starts from 0, 0), so fill with negative infinity
+        for(int x = 1; x < ptr_t_x[bid]; x++) {
+            arr_values[bid][x][0] = -std::numeric_limits<float>::infinity();
+        }
+
+        for(int x = 1; x < ptr_t_x[bid]; x++) {
+            for(int y = 1; y < ptr_t_y[bid]; y++) {
+                arr_values[bid][x][y] += std::max(arr_values[bid][x - 1][y - 1], arr_values[bid][x][y - 1]);
+            }
+        }
+
+        // Restore path
+        int x = ptr_t_x[bid] - 1;
+        for(int y = ptr_t_y[bid] - 1; y >= 0; y--) {
+            arr_path[bid][x][y] = 1.0f;
+            if(x > 0 && y > 0) {
+                if(arr_values[bid][x - 1][y - 1] > arr_values[bid][x][y - 1]) {
+                    x--;
+                }
             }
         }
     }
+    
+    delete[] values_clone;
 
-    return output;
+    return path;
 }
 
 PYBIND11_MODULE(monotonic_alignment_cpp, m) {
     m.doc() = "Monotonic alignment written in C++"; // optional module docstring
 
-    m.def("increment", &increment, "A function that increments all elements of a numpy array by 1", py::arg("input"));
+    m.def(
+        "maximum_path",
+        &maximum_path,
+        "A function that finds a maximum path in a batch of values",
+        py::arg("values"),
+        py::arg("t_x"),
+        py::arg("t_y"),
+        py::return_value_policy::move,
+        R"pbdoc(
+            A function that finds a maximum path in a batch of values
+
+            Args:
+                values (np.ndarray): A batch of values
+                t_x (np.ndarray): A batch of x coordinates of the last point in the path
+                t_y (np.ndarray): A batch of y coordinates of the last point in the path
+
+            Returns:
+                np.ndarray: A batch of paths
+        )pbdoc"
+    );
 }
--- test.py
+++ test.py
@@ -1,14 +1,81 @@
 import monotonic_alignment_cpp
 import numpy as np
+import time
 
-batch_size = 2
-t_x = 4
-t_y = 10
+np.set_printoptions(precision=3, suppress=True)
 
-batch = np.arange(batch_size * t_x * t_y).reshape(batch_size, t_x, t_y).astype(np.float32)
+batch_size = 32
 
-print(batch)
+t_x = np.random.randint(200,400, size=(batch_size,))
+t_y_multipliers = np.random.uniform(1.5, 3.0, size=(batch_size,))
+t_y = (t_x * t_y_multipliers).astype(np.int64)
 
-print(monotonic_alignment_cpp.increment(batch))
+max_t_x = np.max(t_x)
+max_t_y = np.max(t_y)
 
-print(batch)
+batch = np.random.randn(batch_size, max_t_x, max_t_y)
+
+batch -= np.max(batch, axis=(1,2)).reshape(-1,1,1) + 1.0
+
+x_mask = np.arange(max_t_x).reshape(1,-1) < t_x.reshape(-1,1)
+y_mask = np.arange(max_t_y).reshape(1,-1) < t_y.reshape(-1,1)
+
+mask = np.matmul(x_mask.reshape(batch_size, max_t_x, 1), y_mask.reshape(batch_size, 1, max_t_y))
+
+def maximum_path_cpp_wrapper(batch, mask):
+    t_x = mask[:, :, 0].sum(axis=1)
+    t_y = mask[:, 0, :].sum(axis=1)
+
+    return monotonic_alignment_cpp.maximum_path(batch, t_x, t_y)
+
+def maximum_path_numpy(value, mask, max_neg_val=None):
+    """
+    Monotonic alignment search algorithm
+    Numpy-friendly version. It's about 4 times faster than torch version.
+    value: [b, t_x, t_y]
+    mask: [b, t_x, t_y]
+    """
+    if max_neg_val is None:
+        max_neg_val = -np.inf  # Patch for Sphinx complaint
+    value = value * mask
+
+    dtype = value.dtype
+    mask = mask.astype(bool)
+
+    b, t_x, t_y = value.shape
+    direction = np.zeros(value.shape, dtype=np.int64)
+    v = np.zeros((b, t_x), dtype=np.float32)
+    x_range = np.arange(t_x, dtype=np.float32).reshape(1, -1)
+    for j in range(t_y):
+        v0 = np.pad(v, [[0, 0], [1, 0]], mode="constant", constant_values=max_neg_val)[:, :-1]
+        v1 = v
+        max_mask = v1 >= v0
+        v_max = np.where(max_mask, v1, v0)
+        direction[:, :, j] = max_mask
+
+        index_mask = x_range <= j
+        v = np.where(index_mask, v_max + value[:, :, j], max_neg_val)
+    direction = np.where(mask, direction, 1)
+
+    path = np.zeros(value.shape, dtype=np.float32)
+    index = mask[:, :, 0].sum(1).astype(np.int64) - 1
+    index_range = np.arange(b)
+    for j in reversed(range(t_y)):
+        path[index_range, index, j] = 1
+        index = index + direction[index_range, index, j] - 1
+    path = path * mask.astype(np.float32)
+    path = path.astype(dtype)
+    return path
+
+start_time = time.time()
+path_cpp = maximum_path_cpp_wrapper(batch, mask)
+cpp_time = time.time() - start_time
+
+start_time = time.time()
+path_numpy = maximum_path_numpy(batch, mask)
+numpy_time = time.time() - start_time
+
+print(np.allclose(path_cpp, path_numpy))
+
+print(f"CPP time: {cpp_time}")
+print(f"NumPy time: {numpy_time}")
